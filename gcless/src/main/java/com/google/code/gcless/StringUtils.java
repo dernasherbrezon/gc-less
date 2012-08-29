287f7bdd932518969e80106e6ab915cac9a32f2f
dernasherbrezon <dernasherbrezon@8fec1764-b0aa-cd6f-7617-c044b9b62b4f>
2012-08-29 07:24:15 +0000

    git-svn-id: https://gc-less.googlecode.com/svn/trunk@7 8fec1764-b0aa-cd6f-7617-c044b9b62b4f

--- gcless/src/main/java/com/google/code/gcless/StringUtils.java
+++ gcless/src/main/java/com/google/code/gcless/StringUtils.java
@@ -5,36 +5,133 @@ public final class StringUtils {
 	public static Iterable<String> splitBySpace(String str) {
 		return new Splitter(str);
 	}
-	
-	//copy/paste from apache commons-lang
-    public static boolean isBlank(String str) {
-        int strLen;
-        if (str == null || (strLen = str.length()) == 0) {
-            return true;
-        }
-        for (int i = 0; i < strLen; i++) {
-            if ((Character.isWhitespace(str.charAt(i)) == false)) {
-                return false;
-            }
-        }
-        return true;
-    }
-    
-    public static boolean isNotBlank(String str) {
-        int strLen;
-        if (str == null || (strLen = str.length()) == 0) {
-            return false;
-        }
-        for (int i = 0; i < strLen; i++) {
-            if ((Character.isWhitespace(str.charAt(i)) == false)) {
-                return true;
-            }
-        }
-        return false;
-    }
-    
-	private StringUtils() {
-		//do nothing
+
+	/**
+	 * replace tokens from original string with "". For example: original -
+	 * "two tokens hello two tokens worldtwo tokens two tokens" token -
+	 * "two tokens" result - "hello worldtwo tokens"
+	 * 
+	 * @param original
+	 *            - cannot be null
+	 * @param token
+	 * @return
+	 */
+	public static Appendable replaceToken(String original, String token) {
+		if (original == null) {
+			throw new IllegalArgumentException("original string cannot be null");
+		}
+		if (token == null || token.length() == 0) {
+			return new StringBuilder(original);
+		}
+		int tokenLength = token.length();
+		int originalLength = original.length();
+		if (tokenLength > originalLength) {
+			return new StringBuilder(original);
+		}
+
+		StringBuilder result = new StringBuilder();
+		boolean previousCharIsWhitespace = true;
+		char firstChar = token.charAt(0);
+		int lastTokenEndIndex = 0;
+		for (int i = 0; i < originalLength;) {
+			int tokenEndIndex = tokenLength + i;
+			//do no check further. there is not enough chars in original to match token
+			if( tokenEndIndex > originalLength ) {
+				break;
+			}
+			char curChar = original.charAt(i);
+			if (curChar != firstChar) {
+				if (!Character.isWhitespace(curChar)) {
+					previousCharIsWhitespace = false;
+				} else {
+					previousCharIsWhitespace = true;
+				}
+				i++;
+				continue;
+			}
+
+			// whitespace is delimeter for tokens
+			if (!previousCharIsWhitespace) {
+				i++;
+				continue;
+			}
+
+			boolean matched = true;
+			for (int j = 1; j < tokenLength && i + j < originalLength; j++) {
+				curChar = original.charAt(i + j);
+				char tokenChar = token.charAt(j);
+				if (tokenChar != curChar) {
+					matched = false;
+					break;
+				}
+			}
+
+			if (!matched) {
+				i++;
+				continue;
+			}
+
+			// skip token from ending of string
+			if (tokenEndIndex == originalLength) {
+				result.append(original.substring(lastTokenEndIndex, i - 1));
+				lastTokenEndIndex = tokenEndIndex;
+				break;
+			}
+
+			if (Character.isWhitespace(original.charAt(tokenEndIndex))) {
+				// skip token from beginning of string
+				if (i == 0) {
+					lastTokenEndIndex = tokenEndIndex + 1;
+					i += 2;
+					continue;
+				}
+
+				if (lastTokenEndIndex < i - 1) {
+					result.append(original.substring(lastTokenEndIndex, i - 1));
+				}
+				lastTokenEndIndex = tokenEndIndex;
+				i += tokenLength;
+			} else {
+				i++;
+			}
+		}
+
+		// append remaining
+		if (lastTokenEndIndex != originalLength) {
+			result.append(original.substring(lastTokenEndIndex));
+		}
+		return result;
 	}
-	
+
+	// copy/paste from apache commons-lang
+	public static boolean isBlank(String str) {
+		int strLen;
+		if (str == null || (strLen = str.length()) == 0) {
+			return true;
+		}
+		for (int i = 0; i < strLen; i++) {
+			if ((Character.isWhitespace(str.charAt(i)) == false)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public static boolean isNotBlank(String str) {
+		int strLen;
+		if (str == null || (strLen = str.length()) == 0) {
+			return false;
+		}
+		for (int i = 0; i < strLen; i++) {
+			if ((Character.isWhitespace(str.charAt(i)) == false)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private StringUtils() {
+		// do nothing
+	}
+
 }
--- gcless/src/test/java/com/google/code/gcless/StringUtilsTest.java
+++ gcless/src/test/java/com/google/code/gcless/StringUtilsTest.java
@@ -11,12 +11,87 @@ import org.junit.Test;
 public class StringUtilsTest {
 
 	@Test
+	public void replaceTwoInARowTokens() {
+		assertEquals("hello world", StringUtils.replaceToken("hello two tokens two tokens world", "two tokens").toString());
+	}
+	
+	@Test
+	public void replaceAll() {
+		assertEquals("", StringUtils.replaceToken("hello world hello world hello world", "hello world").toString());
+	}
+	
+	@Test
+	public void tokenBiggerThanString() {
+		assertEquals("hello", StringUtils.replaceToken("hello", "hello1").toString());
+	}
+	
+	@Test
+	public void noReplaceStart() {
+		assertEquals("two token hello", StringUtils.replaceToken("two token hello", "two tokens").toString());
+	}
+	
+	@Test
+	public void noReplaceEnd() {
+		assertEquals("hello two token", StringUtils.replaceToken("hello two token", "two tokens").toString());
+	}
+	
+	@Test
+	public void noReplaceMiddle() {
+		assertEquals("hello two token world", StringUtils.replaceToken("hello two token world", "two tokens").toString());
+	}
+	
+	@Test
+	public void replaceStart() {
+		assertEquals("hello", StringUtils.replaceToken("two tokens hello", "two tokens").toString());
+	}
+
+	@Test
+	public void replaceEnd() {
+		assertEquals("hello", StringUtils.replaceToken("hello two tokens", "two tokens").toString());
+	}
+
+	@Test
+	public void replaceMiddle() {
+		assertEquals("hello world", StringUtils.replaceToken("hello two tokens world", "two tokens").toString());
+	}
+
+	@Test
+	public void replaceCombined() {
+		assertEquals("hello world", StringUtils.replaceToken("two tokens hello two tokens world two tokens", "two tokens").toString());
+	}
+
+	@Test
+	public void replaceStartOnlyToken() {
+		assertEquals("two tokenshello", StringUtils.replaceToken("two tokenshello", "two tokens").toString());
+	}
+
+	@Test
+	public void replaceEndOnlyToken() {
+		assertEquals("hellotwo tokens", StringUtils.replaceToken("hellotwo tokens", "two tokens").toString());
+	}
+
+	@Test
+	public void replaceMiddleOnlyToken() {
+		assertEquals("hellotwo tokensworld", StringUtils.replaceToken("hellotwo tokensworld", "two tokens").toString());
+	}
+
+	@Test
+	public void replaceMiddleOnlyToken2() {
+		assertEquals("hello two tokensworld", StringUtils.replaceToken("hello two tokensworld", "two tokens").toString());
+	}
+
+	@Test
+	public void replaceMiddleOnlyToken3() {
+		assertEquals("hellotwo tokens world", StringUtils.replaceToken("hellotwo tokens world", "two tokens").toString());
+	}
+
+	@Test
 	public void splitEmptyString() {
 		Iterable<String> result = StringUtils.splitBySpace("   ");
 		Iterator<String> it = result.iterator();
 		assertFalse(it.hasNext());
 	}
-	
+
 	@Test
 	public void splitNormalString() {
 		Iterable<String> result = StringUtils.splitBySpace(" test  value ");
@@ -27,14 +102,14 @@ public class StringUtilsTest {
 		assertEquals("value", it.next());
 		assertFalse(it.hasNext());
 	}
-	
+
 	@Test
-	public  void splitSingleWord() {
+	public void splitSingleWord() {
 		Iterable<String> result = StringUtils.splitBySpace("test");
 		Iterator<String> it = result.iterator();
 		assertTrue(it.hasNext());
 		assertEquals("test", it.next());
 		assertFalse(it.hasNext());
 	}
-	
+
 }

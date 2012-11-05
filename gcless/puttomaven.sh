523c402eec6160b8d95e458f6b285b072bb04ca0
dernasherbrezon <dernasherbrezon@8fec1764-b0aa-cd6f-7617-c044b9b62b4f>
2012-11-05 15:16:23 +0000

    git-svn-id: https://gc-less.googlecode.com/svn/trunk@15 8fec1764-b0aa-cd6f-7617-c044b9b62b4f

--- gcless/puttomaven.sh
+++ gcless/puttomaven.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/usr/share/maven3/bin/mvn install:install-file -Dfile=./target/gcless-3.0.jar -DpomFile=./pom.xml -DlocalRepositoryPath=../gcless-maven/ -DcreateChecksum=true -Dsources=./target/gcless-3.0-sources.jar
+/usr/share/maven3/bin/mvn install:install-file -Dfile=./target/gcless-4.0.jar -DpomFile=./pom.xml -DlocalRepositoryPath=../gcless-maven/ -DcreateChecksum=true -Dsources=./target/gcless-4.0-sources.jar
